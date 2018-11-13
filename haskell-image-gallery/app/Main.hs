9fc85ed2e0fc5653fe82bf96b4099a331edadd49
Ícaro Pires <icaropsa@gmail.com>
2018-11-12 23:03:00 -0200

    Add migrations

--- haskell-image-gallery/app/Main.hs
+++ haskell-image-gallery/app/Main.hs
@@ -7,19 +7,18 @@ import Model
 
 import Web.Scotty
 import Web.Scotty.Internal.Types (ActionT)
-import Network.Wai
 import Network.Wai.Middleware.Static
-import Network.Wai.Middleware.RequestLogger (logStdoutDev, logStdout)
-import Network.Wai.Middleware.HttpAuth
+import Network.Wai.Middleware.RequestLogger (logStdout)
 import Network.Wai.Middleware.Cors
-import Control.Applicative
 import Control.Monad.IO.Class
 import qualified Data.Configurator as C
 import qualified Data.Configurator.Types as C
-import Data.Pool(Pool, createPool, withResource)
+import Data.Pool(createPool)
 import qualified Data.Text.Lazy as TL
 import Data.Aeson
 import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.Migration
+import qualified Data.ByteString.Char8 as BS8
 
 -- Parse file "application.conf" and get the DB connection info
 makeModelConfig :: C.Config -> IO (Maybe Model.ModelConfig)
@@ -42,6 +41,13 @@ main = do
         Nothing -> putStrLn "No database configuration found, exiting..."
         Just conf -> do
             pool <-  createPool (newConn conf) close 1 40 10
+
+            let url = "host=" ++ (dbHost conf) ++ " dbname=" ++ (dbName conf) ++ " user=" ++ (dbUser conf) ++ " password=" ++ (dbPassword conf)
+            let cmd = MigrationCommands [ MigrationInitialization, MigrationDirectory "postgresql" ]
+            let ctx = MigrationContext cmd False
+
+            con <- connectPostgreSQL (BS8.pack url)
+            _ <- withTransaction con (runMigration (ctx con))
 
             putStrLn "Starting Server..."
             scotty 3000 $ do
@@ -68,20 +74,19 @@ main = do
 
                 -- VIEW
                 get "/image/:id" $ do
-                    id <- param "id" :: ActionM TL.Text
-                    maybeImage <- liftIO $ findImage pool id
+                    local_id <- param "id" :: ActionM TL.Text
+                    maybeImage <- liftIO $ findImage pool local_id
                     viewImage maybeImage
 
 
                 -- DELETE
                 delete "/admin/image/:id" $ do
-                    id <- param "id" :: ActionM TL.Text
-                    deleteImage pool id
-                    deletedImage id
+                    local_id <- param "id" :: ActionM TL.Text
+                    deleteImage pool local_id
+                    deletedImage local_id
 
 -- Parse the request body into the Image
 getImageParam :: ActionT TL.Text IO (Maybe Image)
 getImageParam = do
     b <- body
     return $ (decode b :: Maybe Image)
-    where makeImage s = ""
